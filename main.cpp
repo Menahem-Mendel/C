650e5c757503ec39a4e745e37750c4d116a28210
Mendel <mendelgel@gmail.com>
2019-05-12 10:44:23 +0300

    new

--- main.cpp
+++ main.cpp
@@ -7,14 +7,12 @@ using namespace std;
 
 int main()
 {
-    clock_t x = clock();
-    for (long long i = 0; i < 100000000ll; i++)
-        log(pow(i, 2));
+	clock_t x = clock();
+	for (long long i = 0; i < 100000000ll; i++)
+		log(pow(i, 2));
 
-    x = clock() - x;
+	x = clock() - x;
 
-    cout << (float)clock() / CLOCKS_PER_SEC << endl;
-	int x = 8;
-
-    return 0;
+	cout << (float)clock() / CLOCKS_PER_SEC << endl;
+	return 0;
 }
